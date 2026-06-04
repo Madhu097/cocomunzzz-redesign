0abc672bae07576aaae474e363ef8c4eba1a18cf
Madhu097 <madhukuruva20@gmail.com>
2026-06-04 11:12:39 +0530

    initial changes3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative assets loading for subpath deployments like GitHub Pages
+  base: '/cocomunzzz-redesign/', // GitHub Pages repo sub-path
 })
